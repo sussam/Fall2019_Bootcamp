2f0e5a887fce91745a0363190af457a3d649571d
samanthasu <sus@ufl.edu>
2019-09-16 23:28:08 -0400

    Update JSONtoMongo.js

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -4,10 +4,10 @@
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+  mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Listing = require('./ListingSchema.js'),
+  config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
@@ -23,27 +23,25 @@ mongoose.connect(config.db.uri, { useNewUrlParser: true });
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
 
-fs.readFile('listings.json', 'utf8', function(err, data){
-  if(err) {
+fs.readFile('listings.json', 'utf8', function (err, data) {
+  if (err) {
     throw err;
   }
   /*var listings = JSON.parse(data);
-    listings.entries.forEach(function(listing) {
-      var listingModel = new Listing(listing);
-      listingModel.save(function(err) {
-        if(err) {
-          throw err;
-        }
-      });
-    });*/
-    JSON.parse(data).entries.forEach(function(element) {
-      Listing.create(element);
+  listings.entries.forEach(function (listing) {
+    var listingModel = new Listing(listing);
+    listingModel.save(function (err) {
+      if (err) {
+        throw err;
+      }
     });
+  });*/
+  JSON.parse(data).entries.forEach(function (element) {
+    Listing.create(element);
+  });
 });
 
-process.exit();
-
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+/*
+  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
